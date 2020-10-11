b37b4d6f26676b7220918ce5ad6cbec6ac78845d
Gaby <lubaaenzin@gmail.com>
2020-10-11 04:08:10 -0300

    home layout

---
+++ src/pages/Home/img/index.js
@@ -0,0 +1,12 @@
+import a from "./1.jpg";
+import b from "./2.jpg";
+import c from "./3.jpg";
+import d from "./4.jpg";
+import e from "./5.jpg";
+import f from "./6.jpg";
+import g from "./7.jpg";
+import h from "./8.jpg";
+import j from "./9.jpg";
+import k from "./10.jpg";
+
+export { a, b, c, d, e, f, g, h, j, k };
