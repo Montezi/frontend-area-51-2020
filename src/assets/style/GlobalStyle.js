590c1b35fe937632f8c7df679c7647647a02a5b7
Gaby <lubaaenzin@gmail.com>
2020-10-11 02:20:37 -0300

    fix

--- src/assets/style/GlobalStyle.js
+++ src/assets/style/GlobalStyle.js
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap');
   body {
     padding: 0;
     margin: 0;
